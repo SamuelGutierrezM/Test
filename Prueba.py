9c6d4407a59bee78f69a22f04d74e287c9b353bb
SamuelGutierrezM <137521323+SamuelGutierrezM@users.noreply.github.com>
2023-09-19 17:32:04 -0500

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -2,9 +2,10 @@ class Casa:
     def __init__(self,color,cantidad_pisos):
         self.color = color
         self.cantidad_pisos = cantidad_pisos
-def message_build():
-    casa_blanca = Casa('blanca',4)
-    return f'Tiene una casa de {casa_blanca.cantidad_pisos} de color {casa_blanca.color}'
 
-def test_message_build():
-    assert message_build() == 'Tiene una casa de 4 pisos y de color blanco'
+def message_build():
+    casa_blanca = Casa('blanco',4)
+    return f'Tiene una csa de {casa_blanca.cantidad_pisos} de color {casa_blanca.color}'
+
+def test_mesagge_build():
+    assert message_build() == 'Tiene una csa de 4 de color blanco'
